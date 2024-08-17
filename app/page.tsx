901004c0bde4ff595fd445a9b55482fe2dc553d9
PriyathikGit <techhack237@gmail.com>
2024-08-17 12:45:38 +0530

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import FeaturedCourses from "@/components/FeaturedCourse";
 import HeroSection from "@/components/HeroSection";
+import Testimonials from "@/components/Testimonials";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WavyBackground from "@/components/Instructors";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
+      <Testimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
