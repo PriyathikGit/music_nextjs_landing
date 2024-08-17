b6a422f9251dacb4071fda7212c8d55316a258b9
PriyathikGit <techhack237@gmail.com>
2024-08-17 12:48:39 +0530

    new

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -39,9 +39,7 @@ const page = () => {
               <div className="flex justify-between items-center mt-20">
                 <CardItem
                   translateZ={20}
-                  as={Link}
-                  href="https://twitter.com/mannupaaji"
-                  target="__blank"
+                 
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
                   Try now →
